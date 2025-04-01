01f095617aaa48718244bb9f74947edf81035f58
Rowan00Omar <rowan00omar@gmail.com>
2025-04-02 00:08:42 +0200

    adding some default app constants

---
+++ lib/util/constants/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const double padding = 16.0;
+  static const double margin = 16.0;
+  static const double borderRadius = 12.0;
+
+  static const double iconSize = 24.0;
+  static const double buttonHeight = 50.0;
+}
---
+++ lib/util/constants/app_strings.dart
@@ -0,0 +1,5 @@
+class AppStrings {
+  static const String appTitle = 'Nour W Tazkieh';
+
+  static const String greetingsText = 'Salamualekom';
+}
