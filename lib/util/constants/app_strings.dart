5f275a2ff9b7fa9f709596935cee7dfb89c00958
Rowan00Omar <rowan00omar@gmail.com>
2025-04-02 00:51:33 +0200

    Update app_strings.dart

--- lib/util/constants/app_strings.dart
+++ lib/util/constants/app_strings.dart
@@ -2,4 +2,10 @@ class AppStrings {
   static const String appTitle = 'Nour W Tazkieh';
 
   static const String greetingsText = 'Salamualekom';
+
+  static const String imageBasePath = 'E:/NourWTazkieh-main/assets/images/';
+
+  static const String quranBtnImg = '${imageBasePath}quran.png';
+  static const String azkarBtnImg = '${imageBasePath}azkar.jpg';
+  static const String tsabeehBtnImg = '${imageBasePath}tsabeeh.jpg';
 }
