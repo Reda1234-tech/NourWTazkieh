83d0564777fda0805ae0331af093085aa6b35575
Rowan00Omar <rowan00omar@gmail.com>
2025-04-02 02:07:18 +0200

    Update app_constants.dart

--- lib/util/constants/app_constants.dart
+++ lib/util/constants/app_constants.dart
@@ -1,10 +1,15 @@
 class AppConstants {
-  static const double padding = 16.0;
+  static const double padding = 8.0;
+  static const double paddinglarge = 16.0;
+
   static const double margin = 16.0;
   static const double borderRadius = 12.0;
+
   static const double sepHeightlow = 5.0;
   static const double sepHeighthigh = 20.0;
 
   static const double iconSize = 24.0;
   static const double buttonHeight = 50.0;
+
+  static const double sectionHeight = 150.0;
 }
